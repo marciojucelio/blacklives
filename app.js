4977190e806b2051c1c16b2d9340f33e8bf82039
Marcio Jucelio <marciojucelio@gmail.com>
2024-09-07 17:20:16 -0300

    Commit Final

--- app.js
+++ app.js
@@ -1,26 +1,40 @@
 function pesquisar() {
     // Inicializa uma string vazia para armazenar os resultados da pesquisa.
     let resultados = "";
-  
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
     // Obtém a seção HTML onde os resultados serão exibidos.
     let section = document.getElementById("resultados-pesquisa");
-    
+    let campoPesquisa = document.getElementById("campoDePesquisa").value;
+    campoPesquisa = campoPesquisa.toLowerCase();
+    campoPesquisa = campoPesquisa.trim();
     // Itera sobre cada item de dados (assumindo que 'dados' é um array).
-    for (let dado of dados) {
-      // Concatena HTML para criar um novo elemento de resultado.
-      // A template literal (``) permite a interpolação de variáveis.
-      resultados += `
-        <div class="item-resultado">
-          <h2> <a href="#" target="_blank">${dado.titulo}</a></h2>
-          <p class="descricao-meta">${dado.descricao}</p>
-          <div class="centralizar-mais-informacoes">
-            <a href=${dado.link} target="_blank">Mais informações</a>
-          </div>
-        </div>
-      `;
+    if (!campoPesquisa) {
+      section.innerHTML = "<h4>Você precisa pesquisar algo.</h4>";
+      return
+    }else {
+      for (let dado of dados) {
+        titulo = dado.titulo.toLocaleLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLowerCase()
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)){
+          // Concatena HTML para criar um novo elemento de resultado.  
+          resultados += `
+            <div class="item-resultado">
+              <h2> <a href="#" target="_blank">${dado.titulo}</a></h2>
+              <p class="descricao-meta">${dado.descricao}</p>
+              <div class="centralizar-mais-informacoes"> 
+                <a href=${dado.link} target="_blank">Mais informações</a>
+              </div>
+            </div>
+          `;
+        };
+      };
+    };
+    if (!resultados) {
+      resultados = "<h4> Nada encontrado </h4>"
     }
-  
     // Atribui a string com os resultados ao conteúdo HTML da seção.
     section.innerHTML = resultados;
-    
   } 
--- dados.js
+++ dados.js
@@ -2,16 +2,19 @@ const dados = [
             {
             titulo: "Martin Luther King Jr.",
             descricao: "Martin Luther King Jr. foi um pastor protestante e ativista afro-americano que emergiu como o principal líder do movimento pelos direitos civis nos Estados Unidos nos anos 1950 e 1960. Através de sua pregação não violenta e de sua capacidade de mobilizar multidões, King liderou campanhas icônicas, como o boicote aos ônibus em Montgomery e a marcha em Washington, onde proferiu seu famoso discurso 'I Have a Dream'. Seu assassinato em 1968 chocou o mundo e solidificou seu lugar na história como um ícone da luta pela igualdade racial.",
-            link: "https://pt.wikipedia.org/wiki/Martin_Luther_King_Jr."
+            link: "https://pt.wikipedia.org/wiki/Martin_Luther_King_Jr.",
+            tags: "ativista, luta, racismo, afro"
             },
             {
-            titulo: "Malcom X",
+            titulo: "Malcolm X",
             descricao: "Malcolm X foi um ativista afro-americano que se tornou uma das vozes mais influentes do movimento pelos direitos civis nos Estados Unidos. Nascido como Malcolm Little, ele adotou o nome Malcom X após se converter ao Islã. Conhecido por suas posições radicais e discursos inflamados, Malcom X defendia a autodeterminação dos afro-americanos e a separação racial. Sua vida e legado continuam a inspirar debates sobre raça, identidade e justiça social.",
-            link: "https://pt.wikipedia.org/wiki/Malcolm_X"
+            link: "https://pt.wikipedia.org/wiki/Malcolm_X",
+            tags: "ativista, luta, racismo, afro"
             },
             {
             titulo: "George Floyd",
             descricao: "George Floyd foi um homem afro-americano cuja morte durante uma prisão em Minneapolis, Minnesota, em 2020, desencadeou protestos globais contra a brutalidade policial e o racismo sistêmico. A morte de Floyd, capturada em vídeo, mostrou um policial branco pressionando seu joelho no pescoço de Floyd por vários minutos, apesar dos pedidos do homem para respirar. O caso de Floyd se tornou um catalisador para o movimento Black Lives Matter e reavivou o debate sobre a justiça racial nos Estados Unidos.",
-            link: "https://pt.wikipedia.org/wiki/George_Floyd"
+            link: "https://pt.wikipedia.org/wiki/George_Floyd",
+            tags: "morte, racismo, crueldade"
             }
     ]
